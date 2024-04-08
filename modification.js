b474b7acab1a54f58290a3202f63cf3186cdd390
Nikolay Edigaryev <edigaryev@gmail.com>
2024-04-08 19:39:31 +0400

    Remove TypeScript remnants as we're now patching vanilla JS directly

--- modification.js
+++ modification.js
@@ -3,7 +3,7 @@ const httpCacheHost = process.env["CIRRUS_HTTP_CACHE_HOST"];
 if (httpCacheHost) {
     const newActionsCacheURL = "http://" + httpCacheHost + "/";
 
-    console.log("Redefining the ACTIONS_CACHE_URL to " + ${newActionsCacheURL} + " to make the cache faster...");
+    console.log("Redefining the ACTIONS_CACHE_URL to " + newActionsCacheURL + " to make the cache faster...");
 
     process.env["ACTIONS_CACHE_URL"] = newActionsCacheURL;
 }
