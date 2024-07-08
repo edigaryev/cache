f7b06c9bd2ffda2a2a060e65c98e7cb8a26d0408
Nikolay Edigaryev <edigaryev@gmail.com>
2024-07-08 16:35:34 +0400

    Support Chacha zone-local cache servers (#4)
    * Support Chacha zone-local cache servers
    
    * Use JS modules to have await at the top-level
    
    Otherwise there's a race introduced.
    
    * Proper fallback when no ACTIONS_ID_TOKEN_REQUEST_* variables are present

--- modification.js
+++
@@ -1,9 +0,0 @@
-const httpCacheHost = process.env["CIRRUS_HTTP_CACHE_HOST"];
-
-if (httpCacheHost) {
-    const newActionsCacheURL = "http://" + httpCacheHost + "/";
-
-    console.log("Redefining the ACTIONS_CACHE_URL to " + newActionsCacheURL + " to make the cache faster...");
-
-    process.env["ACTIONS_CACHE_URL"] = newActionsCacheURL;
-}
